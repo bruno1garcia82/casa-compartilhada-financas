1ee5a9390fb3da451b9eb25514a9c5e1735ed0da
bruno1garcia82 <garcia1bruno82@gmail.com>
2025-07-31 13:16:30 -0300

    Create users.ts

---
+++ src/constants/users.ts
@@ -0,0 +1,11 @@
+// src/constants/users.ts
+export const USERS = {
+  JULIA: {
+    id: 'c92280ad-49b4-4c5d-8e4f-96cfb6e55ae6', // Ex: 'a1b2c3d4-1234-5678-9012-abcdef123456'
+    name: 'Julia Stuart'
+  },
+  BRUNO: {
+    id: '1ebb5f85-cb57-471c-8548-002d89ec0007',
+    name: 'Bruno Garcia'
+  }
+} as const;
